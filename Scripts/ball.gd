4a41ac5e4e552404f3807a1ac326ae78c657401d
Cosme Manuel Tejeda <cosme.m.tejeda@gmail.com>
2023-12-03 17:57:27 -0600

    Ball Manipulation Complete
    * Can now control ball movement.
    * Created ball and brick scenes.
    * Can respawn ball.

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,21 +1,25 @@
 extends RigidBody2D
 
-const SPEEDUP = 4
-const MAXSPEED = 300
+const SPEEDUP = 400
+const MAXSPEED = 30000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_process(true)
+	set_physics_process(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var bodies = get_colliding_bodies()
 	
 	for body in bodies:
 		if body.is_in_group("Bricks"):
 			body.queue_free()
 			
-		elif body == get_node("../Paddle"):
+		elif body.name == "Paddle":
 			var speed = linear_velocity.length()
-			#var direction = 
-		
+			var direction = position - body.get_node("Anchor").global_position
+			var velocity = direction.normalized() * min(speed+SPEEDUP*delta, MAXSPEED*delta)
+			linear_velocity = velocity
+	
+	if position.y > get_viewport_rect().end.y:
+		queue_free()
